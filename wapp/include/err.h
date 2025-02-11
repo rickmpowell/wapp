774167bc7f8ba6c89c5818f0164a4bedb32efc48
Richard Powell <rickpo@msn.com>
2025-02-10 23:40:14 -0500

    Documentation

--- wapp/include/err.h
+++ wapp/include/err.h
@@ -3,12 +3,20 @@
 /*
  *  err.h
  * 
- *  Errors
+ *  Errors. These can be thrown during an exception which can then be used
+ *  to display an error message. 
  */
 
 #include "framework.h"
 
 const unsigned facilityApp = 0x0100;
+
+/*
+ *  ERR class
+ * 
+ *  The simplest error type, represents an HRESULT returned by a Windows
+ *  API. 
+ */
 
 class ERR
 {
@@ -24,17 +32,36 @@ public:
     
 };
 
+/*
+ *  ERRAPP
+ * 
+ *  An app-specific error, where the code represents a string ID in the
+ *  resource file. For more complex errors, these types of errors can take
+ *  an "argument" that is inserted into the string wherever the sub-string
+ *  {} lives.
+ */
+
 class ERRAPP : public ERR
 {
 public:
     ERRAPP(int rss, const wstring& wsArg = L"") : ERR(MAKE_HRESULT(1, facilityApp, rss), wsArg) {}
 };
 
+/*
+ *  ERRLAST
+ *  
+ *  An error that is taken from the Windows GetLastError.
+ */
+
 class ERRLAST : public ERR
 {
 public:
     ERRLAST(void) : ERR(::GetLastError()) {}
 };
+
+/*
+ *  A few standard commonly used errors
+ */
 
 const ERR errNone = ERR(S_OK);
 const ERR errFail = ERR(E_FAIL);
