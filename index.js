df7f4500f2d3fda1bf797686df472bf1cf7f989d
rickmpowell <rickmpowell@users.noreply.github.com>
2025-08-17 04:00:35 +0000

    deploy: bf1383af103c24d5ca76d8eba994d1c83ea38c9d

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var index =
     [ "What it is", "index.html#autotoc_md1", null ],
     [ "Requirements", "index.html#autotoc_md2", null ],
     [ "To create a new project", "index.html#autotoc_md3", null ],
-    [ "Documentation", "index.html#autotoc_md4", null ]
+    [ "Documentation", "index.html#autotoc_md4", null ],
+    [ "Functionality", "index.html#autotoc_md5", null ]
 ];
--- navtreeindex0.js
+++ navtreeindex0.js
@@ -173,5 +173,6 @@ var NAVTREEINDEX0 =
 "index.html#autotoc_md2":[0,1],
 "index.html#autotoc_md3":[0,2],
 "index.html#autotoc_md4":[0,3],
+"index.html#autotoc_md5":[0,4],
 "pages.html":[]
 };
